6f41c7e3e1aa111b50c49864611d46e599427bf3
Treborsky <rob.koziarski2k@gmail.com>
2022-11-10 07:24:05 +0000

    Initial commit - simple hello world
    TODO:
     - zephyr-compatible main.c source file
     - upload it to git

---
+++ main.c
@@ -0,0 +1,17 @@
+// 
+// Project: STM32F411RE single gpio comm
+//
+// Author: Treborsky, rob.koziarski2k@gmail.com
+//
+// File: main.c
+//
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    printf("Hello World!\n");
+    return 0;
+}
+
+
+
